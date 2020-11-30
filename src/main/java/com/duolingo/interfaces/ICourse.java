521a3b6251c138b1119cb4ff2847302ab298e33f
jorge-tabuenca <jorgetabuenca99@gmail.com>
2020-11-30 18:10:22 +0100

    Modificaciones Lib

---
+++ src/main/java/com/duolingo/interfaces/ICourse.java
@@ -0,0 +1,12 @@
+package com.duolingo.interfaces;
+
+import java.util.List;
+
+import com.duolingo.model.Course;
+import com.duolingo.model.Language;
+
+public interface ICourse {
+	
+	public List<Course> getAllCourses();
+
+}
